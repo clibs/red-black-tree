8ec7de1b92362a1ac9b91e74756f672a211dee70
JesperBentzen <50871036+JesperBentzen@users.noreply.github.com>
2019-05-22 10:56:41 +0200

    Update jsw_rbtree.c
    Added checks to ensures we do not don't count size down if we didn't erase anything, and added checks to ensure we only count the size up if we added a node (adding a duplicate would previously cause the count to be counted up, and removing a none existant item would count the size down).

--- src/jsw_rbtree.c
+++ src/jsw_rbtree.c
@@ -236,12 +236,14 @@ void *jsw_rbfind ( jsw_rbtree_t *tree, void *data )
 */
 int jsw_rbinsert ( jsw_rbtree_t *tree, void *data )
 {
+  int added = 0; // keeps track of if we added a node or not
   if ( tree->root == NULL ) {
     /*
       We have an empty tree; attach the
       new node directly to the root
     */
     tree->root = new_node ( tree, data );
+    added = 1; // remember we added a node
 
     if ( tree->root == NULL )
       return 0;
@@ -262,6 +264,7 @@ int jsw_rbinsert ( jsw_rbtree_t *tree, void *data )
       if ( q == NULL ) {
         /* Insert a new node at the first null link */
         p->link[dir] = q = new_node ( tree, data );
+        added = 1; // remember we added a node
 
         if ( q == NULL )
           return 0;
@@ -307,9 +310,10 @@ int jsw_rbinsert ( jsw_rbtree_t *tree, void *data )
 
   /* Make the root black for simplified logic */
   tree->root->red = 0;
-  ++tree->size;
+  if(added) // did we add a node?
+    ++tree->size; // then count up
 
-  return 1;
+  return added; // return if we added a node or not
 }
 
 /**
@@ -399,7 +403,7 @@ int jsw_rberase ( jsw_rbtree_t *tree, void *data )
       p->link[p->link[1] == q] =
         q->link[q->link[0] == NULL];
       free ( q );
-    }
+    //Moved}
 
     /* Update the root (it may be different) */
     tree->root = head.link[1];
@@ -408,7 +412,8 @@ int jsw_rberase ( jsw_rbtree_t *tree, void *data )
     if ( tree->root != NULL )
       tree->root->red = 0;
 
-    --tree->size;
+    --tree->size; // only count down if we erased a node
+    }
   }
 
   return 1;
